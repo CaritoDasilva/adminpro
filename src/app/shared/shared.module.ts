1fc933ba288cfb69fdd11cac8c38e21a4b377fef
CaritoDasilvagit <caritodasilva@gmail.com>
2018-11-12 13:48:07 -0300

    se sube actualización de rutas y modules

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
+import { BreadcrumsComponent } from './breadcrums/breadcrums.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
+
+
+
+
+@NgModule({
+    declarations: [
+        HeaderComponent,
+        SidebarComponent,
+        BreadcrumsComponent,
+        NopagefoundComponent,
+    ],
+    exports: [
+        HeaderComponent,
+        SidebarComponent,
+        BreadcrumsComponent,
+        NopagefoundComponent
+    ]
+})
+
+export class SharedModule { }
