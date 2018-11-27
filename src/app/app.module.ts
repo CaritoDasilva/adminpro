4dee531f46381fe46b533d8717e5cb790588b9f3
CaritoDasilvagit <caritodasilva@gmail.com>
2018-11-27 20:44:47 -0300

    Se agregan graficos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 // Rutas
-import { APP_ROUTES } from './app.routes';
+import { APP_ROUTES } from "./app.routes";
 
 // Modulos
-import { PagesModule } from './pages/pages.module';
+import { PagesModule } from "./pages/pages.module";
 
 // Componentes
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent,
-  ],
+  declarations: [AppComponent, LoginComponent, RegisterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     APP_ROUTES,
     PagesModule,
-
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
