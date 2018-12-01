d8036a75b1cbaed09c5c353cf58191e1fb607b0d
CaritoDasilvagit <caritodasilva@gmail.com>
2018-11-30 22:26:51 -0300

    ejercicios de promesas y observables en el proyecto

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,18 +1,20 @@
-import { NgModule } from "@angular/core";
-import { DashboardComponent } from "./dashboard/dashboard.component";
-import { ProgressComponent } from "./progress/progress.component";
-import { Graficas1Component } from "./graficas1/graficas1.component";
-import { PagesComponent } from "./pages.component";
-import { SharedModule } from "../shared/shared.module";
-import { PAGES_ROUTES } from "./pages.routes";
-import { FormsModule } from "@angular/forms";
+import { NgModule } from '@angular/core';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { PagesComponent } from './pages.component';
+import { SharedModule } from '../shared/shared.module';
+import { PAGES_ROUTES } from './pages.routes';
+import { FormsModule } from '@angular/forms';
 // ng2 Charts
-import { ChartsModule } from "ng2-charts";
+import { ChartsModule } from 'ng2-charts';
 
 // Temporales
-import { IncrementadorComponent } from "../components/incrementador/incrementador.component";
-import { GraficoDonaComponent } from "../components/grafico-dona/grafico-dona.component";
-import { AccountSettingsComponent } from "./account-settings/account-settings.component";
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +24,9 @@ import { AccountSettingsComponent } from "./account-settings/account-settings.co
     Graficas1Component,
     IncrementadorComponent,
     GraficoDonaComponent,
-    AccountSettingsComponent
+    AccountSettingsComponent,
+    PromesasComponent,
+    RxjsComponent
   ],
   exports: [
     PagesComponent,
